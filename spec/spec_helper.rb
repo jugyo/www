0c978a90e038a28190d3d08522d5c7fc4a1a6c9a
jugyo <jugyo.org@gmail.com>
2010-10-15 02:16:16 +0900

    add specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,10 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'www'
 require 'rspec'
 require 'rspec/autorun'
+require 'rack/test'
+
+include Rack::Test::Methods
 
 RSpec.configure do |config|
-  
+  config.mock_with :rr
 end
--- spec/www_spec.rb
+++ spec/www_spec.rb
@@ -1,7 +1,66 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+class Foo < Www::Base
+  view_dir File.join(File.dirname(__FILE__), 'views')
+
+  before {}
+  before :foo_required
+
+  get '/'
+  def index
+    "index"
+  end
+
+  get '/foo'
+  def foo(params)
+    haml :params => params
+  end
+
+  get '/regexp/?(.*)'
+  def regexp(arg, params)
+    "#{arg} - #{params.inspect}"
+  end
+
+  get '/(\d{4})/(\d{2})/(\d{2})'
+  def entry(year, month, date)
+    [year, month, date]
+  end
+
+  def foo_required
+  end
+end
+
+def app
+  Www::App
+end
 
 describe "Www" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it 'should call before block' do
+    foo = Foo.new
+    2.times do
+      mock.proxy(foo).foo_required
+      mock.proxy(foo).instance_eval(&Foo.before_blocks[0])
+      foo.index
+    end
+  end
+
+  it 'should call index as "/"' do
+    get('/').body.should == "index"
+  end
+
+  it 'should call foo as "/foo" with params' do
+    params = {'foo' => 'bar'}
+    get('/foo', params).body.chomp.should == params.inspect
+  end
+
+  it 'should call regexp as "/regexp/..."' do
+    get('/regexp/foo').body.should == "foo - #{{}.inspect}"
+  end
+
+  it 'should call regexp as "/regexp/..." with params' do
+    params = {'foo' => 'bar'}
+    get('/regexp/foo', params).body.should == "foo - #{params.inspect}"
+  end
+
+  it 'should call entry as "/2010/10/15"' do
+    get('/2010/10/15').body.should == ['2010', '10', '15'].inspect
   end
 end
