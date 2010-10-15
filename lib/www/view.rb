562de30ed8cb0ab8d51ad2b0c85f76022c92415d
jugyo <jugyo.org@gmail.com>
2010-10-16 01:20:30 +0900

    support all tilt supports

--- lib/www/view.rb
+++ lib/www/view.rb
@@ -19,10 +19,16 @@ module Www
       end
     end
 
-    def haml(*args)
+    Tilt.mappings.keys.map { |key| key.to_sym }.each do |name|
+      define_method(name) do |*args|
+        render name, *args
+      end
+    end
+
+    def render(type, *args)
       name = args[0].is_a?(String) ? args.shift : @route_name
       values = args[0]
-      path = File.join(self.class.view_dir_path, "#{name}.haml")
+      path = File.join(self.class.view_dir_path, "#{name}.#{type}")
       template = Tilt.new(path)
       template.render self, values # TODO: helper class instead of Object.new
     end
